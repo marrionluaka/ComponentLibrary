1155dd71c861f297f0ddf4d6462a3db4d4c5d2d4
Marrion Lulu <marrion91300@gmail.com>
2023-02-07 19:49:07 -0800

    Capturing tweet events

--- src/components/Tweet/Tweet.stories.js
+++ src/components/Tweet/Tweet.stories.js
@@ -3,15 +3,31 @@ import tweetData from './data'
 
 export default {
   title: 'Example/Tweet',
-  component: Tweet
+  component: Tweet,
+  argTypes: {
+    onCommentClicked: { action: 'Comment clicked!' },
+    onRetweetClicked: { action: 'Retweet clicked!' },
+    onLikeClicked: { action: 'Like clicked!' },
+    onShareClicked: { action: 'Share clicked!' }
+  }
 }
 
-const Template = () => ({
+const Template = args => ({
   template: `
-    <Tweet v-bind="args"></Tweet>
+    <Tweet
+      v-bind="args"
+      @on-comment-clicked="args.onCommentClicked"
+      @on-retweet-clicked="args.onRetweetClicked"
+      @on-like-clicked="args.onLikeClicked"
+      @on-share-clicked="args.onShareClicked"
+    />
   `,
 
   components: { Tweet },
+
+  setup() {
+    return { args }
+  }
 })
 
 export const Primary = Template.bind({})
