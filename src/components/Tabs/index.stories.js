b073bcb0d68f3cbd463c9eb591a400d9f472b3dd
Marrion Lulu <marrion91300@gmail.com>
2023-02-21 21:52:49 -0800

    Cleaning up story

--- src/components/Tabs/index.stories.js
+++ src/components/Tabs/index.stories.js
@@ -1,6 +1,7 @@
 import Tab from './Tab.vue'
 import Tabs from './index.vue'
 import TabPanel from './TabPanel.vue'
+import { computed, ref } from 'vue'
 
 export default {
   title: 'Example/Tabs',
@@ -13,41 +14,23 @@ const Template = () => ({
       <div class="flex gap-x-2">
         <Tab :id="0" :selected="true">
           <template #default="{ active, setActiveTabID }">
-            <button
-              :class="[
-                'appearance-none border border-gray-300 rounded-md py-1 px-3 text-sm',
-                active ? 'bg-indigo-500 border-indigo-500 text-white' : 'text-gray-900 border-gray-300'
-              ]"
-              @click="setActiveTabID(0)"
-            >HTML</button>
+            <button :class="getClasses(active)" @click="setActiveTabID(0)">HTML</button>
           </template>
         </Tab>
 
         <Tab :id="1">
           <template #default="{ active, setActiveTabID }">
-            <button
-              :class="[
-                'appearance-none border border-gray-300 rounded-md py-1 px-3 text-sm',
-                active ? 'bg-indigo-500 border-indigo-500 text-white' : 'text-gray-900 border-gray-300'
-              ]"
-              @click="setActiveTabID(1)"
-            >CSS</button>
+            <button :class="getClasses(active)" @click="setActiveTabID(1)">CSS</button>
           </template>
         </Tab>
 
         <Tab :id="2">
           <template #default="{ active, setActiveTabID }">
-            <button
-              :class="[
-                'appearance-none border border-gray-300 rounded-md py-1 px-3 text-sm',
-                active ? 'bg-indigo-500 border-indigo-500 text-white' : 'text-gray-900 border-gray-300'
-              ]"
-              @click="setActiveTabID(2)"
-            >JavaScript</button>
+            <button :class="getClasses(active)" @click="setActiveTabID(2)">JavaScript</button>
           </template>
         </Tab>
 
-        <Tab as="button" class="border border-gray-300 rounded-md py-1 px-3 text-sm bg-gray-100 text-gray-500" :id="3" :disabled="true">
+        <Tab as="button" :class="[tabClass, 'bg-gray-100 text-gray-500']" :id="3" :disabled="true">
           Haskell
         </Tab>
       </div>
@@ -72,6 +55,19 @@ const Template = () => ({
   `,
 
   components: { Tabs, Tab, TabPanel },
+
+  setup() {
+    const tabClass = ref('border border-gray-300 rounded-md py-1 px-3 text-sm')
+
+    function getClasses(active) {
+      return [
+        `appearance-none ${tabClass.value}`,
+        active ? 'bg-indigo-500 border-indigo-500 text-white' : 'text-gray-900 border-gray-300'
+      ]
+    }
+
+    return { tabClass, getClasses }
+  }
 })
 
 export const Primary = Template.bind({})
