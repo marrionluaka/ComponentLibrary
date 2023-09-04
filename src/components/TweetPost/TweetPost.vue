223996077c26ed00e57a9655b59bf68330fa14f1
Marrion Lulu <marrion91300@gmail.com>
2023-09-04 10:53:01 -0700

    refactoring tweet post

--- src/components/TweetPost/TweetPost.vue
+++ src/components/TweetPost/TweetPost.vue
@@ -1,18 +1,31 @@
-<template lang="pug">
-div(class="max-w-xl" data-test="tweet")
-  form(@submit.prevent="$emit('on-tweet', tweetBox)")
-    .flex.justify-items-end.rounded-md.border.p-3.mb-2
-      textarea.w-full.resize-none.outline-none.border-0.mr-2(class="focus:ring-0" rows="4" role="textbox" data-test="tweet-box" v-model="tweetBox")
-      .flex.flex-col.justify-between
-        Smiley.cursor-pointer
-        span(data-test="tweet-limit") {{ tweetLimit }}
-    button.py-2.px-4.bg-purple-500.text-white.font-semibold.rounded-full(
-      type="submit"
-      data-test="tweet-submit"
-      class="focus:outline-none disabled:opacity-50"
-      :class="[isDisabled ? 'hover:bg-purple-500' : 'hover:bg-purple-400', { 'cursor-not-allowed': isDisabled }]"
-      :disabled="isDisabled"
-    ) Tweet
+<template>
+  <div class="max-w-xl" data-test="tweet">
+    <form @submit.prevent="$emit('on-tweet', tweetBox)">
+      <div class="flex justify-items-end rounded-md border p-3 mb-2">
+        <textarea
+          class="w-full resize-none outline-none border-0 mr-2 focus:ring-0"
+          rows="4"
+          role="textbox"
+          data-test="tweet-box"
+          v-model="tweetBox"
+        />
+        <div class="flex flex-col justify-between">
+          <Smiley class="cursor-pointer" />
+          <span data-test="tweet-limit">{{ tweetLimit }}</span>
+        </div>
+      </div>
+
+      <button
+        class="py-2 px-4 bg-purple-500 text-white font-semibold rounded-full focus:outline-none disabled:opacity-50"
+        type="submit"
+        data-test="tweet-submit"
+        :class="[isDisabled ? 'hover:bg-purple-500' : 'hover:bg-purple-400', { 'cursor-not-allowed': isDisabled }]"
+        :disabled="isDisabled"
+      >
+        Tweet
+      </button>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
